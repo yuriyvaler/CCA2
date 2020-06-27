96695854a4e1c23aa01588a732bf6b8e746d9b6c
babar <yuriyvaler@gmail.com>
2020-06-27 12:32:51 -0400

    Add changes

--- wdio.conf.js
+++ wdio.conf.js
@@ -17,7 +17,8 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/*.js'
+        './test/elements.js',
+        './test/defaultfunctionality.js'
     ],
     // Patterns to exclude.
     exclude: [
